2e53b713502eb85a505bcc74c099174bf902f719
Rich Martinez <richmartinez@quickenloans.com>
2017-08-10 17:56:05 -0400

    Add create react app, styled components, and update app component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div>
+    <span>Row1</span>
+    <span>Row2</span>
+  </div>
+);
+
+export default App;
